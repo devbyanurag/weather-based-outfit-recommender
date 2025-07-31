04dbab7f456a2dd9d6d78edd1a4630ffe2d26c31
devbyanurag <karanchheti@gmail.com>
2025-07-31 23:24:52 +0530

    depoyment fix for secure request

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,4 @@
 export const BASE_URL_OPENWEATHERMAP="https://api.openweathermap.org/data/2.5/"
 export const BASE_URL_OPENWEATHERMAP_ICON="https://openweathermap.org/"
 
-export const BASE_URL_GEO_OPENWEATHERMAP="http://api.openweathermap.org/geo/1.0/"
+export const BASE_URL_GEO_OPENWEATHERMAP="https://api.openweathermap.org/geo/1.0/"
